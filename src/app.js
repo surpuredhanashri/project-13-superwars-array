752f9a64b792b900d69c3e2202556131543a8706
surpuredhanashri <surpuredhanashri@gmail.com>
2020-10-01 02:50:52 +0530

    DONE

--- src/app.js
+++ src/app.js
@@ -21,62 +21,56 @@ const PLAYERS = [
     "Slingo"
 ];
 
-var i = 0;
+var i = 1;
 // initialize players with image and strength
-const initPlayers = players => {
-    let detailedPlayers = "";
+const initPlayers = (PLAYERS) => {
+    let playerDetails = '';
 
     // Instead of forloop use Map method
     // Code here
-    detailedPlayers = players.map((player, i) => {
+    playerDetails = PLAYERS.map((player,i) => {
         console.log(i);
         return {
             name: player,
-            image: "images/super-" + (i + 1) + ".png",
+            image: "images/super-" + (i) + ".png",
             strength: getRandomStrength(),
-            type: i % 2 == 0 ? "hero" : "villain"
-        };
+            type: (i%2 == 0)?"hero":"villain"
+        }
     });
 
-    return detailedPlayers;
-};
+    return playerDetails;
+}
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-};
+    return Math.floor(Math.random() * 100);
+}
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = "";
+    let temp = '';
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    
 
-    fragment = players
-        .filter(player => player.type == type)
-        .map(
-            player =>
-            `<div class="player">
+    temp = players.filter(player => player.type == type).map(player =>
+    `<div class="player">
     <img src="${player.image}" alt=" ">
     <div class="name">${player.name}</div>
     <div class="strength">${player.strength}</div>
-</div>`
-        )
-        .join();
-    return fragment;
-};
+    </div>`).join();
+    return temp;
+}
 
 // Display players in HTML
-const viewPlayers = players => {
-    document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
-    document.getElementById("villains").innerHTML = buildPlayers(
-        players,
-        "villain"
-    );
-};
+const viewPlayers = (players) => {
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+}
+
 
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
-};
+}
